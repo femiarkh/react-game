8070250abb85989c3c5bfff6fe88fa69a9e73f91
Dmitry Gutkovsky <femiarkh@gmail.com>
2021-02-22 10:58:36 +0300

    feat: add correct input check

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Letter } from './Letter';
 import { useArray } from '../hooks/useArray';
 import { useGameSize } from '../hooks/useGameSize';
 import { GAME_SIZES } from '../const/GAME_SIZES';
 
-
 const GameBoard = () => {
   const { array } = useArray();
   const { gameSize } = useGameSize();
+  const initialUsedIndexes = new Array(gameSize)
+    .fill((array.length - gameSize) / 2)
+    .map((item, index) => item + index);
+  const [usedIndexes, setUsedIndexes] = useState(initialUsedIndexes);
 
   const letters = array
-    .map((item) => <Letter
+    .map((item, index) => <Letter
       key={item.id}
       index={+item.id}
       value={item.value}
-      array={array}
+      disabled={usedIndexes.includes(index)}
+      usedIndexes={usedIndexes}
+      setUsedIndexes={setUsedIndexes}
     />);
 
   return (
--- src/components/Letter.tsx
+++ src/components/Letter.tsx
@@ -1,24 +1,58 @@
 import React from 'react';
 import { useArray } from '../hooks/useArray';
+import { useGameSize } from '../hooks/useGameSize';
 
 type Props = {
   value: string;
   index: number;
-  array: {
-    value: string;
-    id: string;
-  }[];
+  disabled: boolean;
+  usedIndexes: number[];
+  setUsedIndexes: React.Dispatch<React.SetStateAction<number[]>>;
 };
 
-const Letter = ({ value, index, array }: Props) => {
-  const { changeArray } = useArray();
+function isInputPossible(index: number, array: {
+  value: string;
+  id: string;
+}[], size: number) {
+  if (array[index - size] && array[index - size].value !== ''
+  || array[index + size] && array[index + size].value !== ''
+  || array[index + 1] && (array[index + 1].value !== '' && (index + 1) % size !== 0)
+  || array[index - 1] && (array[index - 1].value !== '' && index % size !== 0)) return true;
+  return false;
+}
 
-  function changeHandler(evt: React.ChangeEvent<HTMLInputElement>) {
+function isInputLegit(input: string) {
+  return input.trim().match(/^[А-Яа-я]$/);
+}
+
+const Letter = ({
+  value, index, disabled, usedIndexes, setUsedIndexes }: Props) => {
+  const { array, changeArray } = useArray();
+  const { gameSize } = useGameSize();
+
+  function handleBlur(evt: React.ChangeEvent<HTMLInputElement>) {
+    if (isInputPossible(index, array, gameSize)
+     && isInputLegit(evt.target.value)) {
+      setUsedIndexes(usedIndexes.concat([index]));
+      return;
+    }
+    if (!isInputLegit(evt.target.value)) {
+      console.log('Input is not legit. Please enter only one letter in Russian.');
+    } else {
+      console.log('Sorry, input is not possible here');
+    }
     const newArray = Array.from(array);
-    newArray[index].value = evt.target.value;
+    newArray[index].value = '';
     changeArray(newArray);
   }
-  return <input className="letter" type="text" value={value} onChange={changeHandler} />;
+
+  function handleChange(evt: React.ChangeEvent<HTMLInputElement>) {
+    const newArray = Array.from(array);
+    newArray[index].value = evt.target.value.toUpperCase();
+    changeArray(newArray);
+  }
+
+  return <input className="letter" type="text" onChange={handleChange} value={value} onBlur={handleBlur} disabled={disabled} />;
 };
 
 export { Letter };
